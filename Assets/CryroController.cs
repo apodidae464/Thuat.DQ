305c8a1ca65f7af0fc8280950fd67021d2456e44
apodidae464 <84320580+apodidae464@users.noreply.github.com>
2021-06-09 15:25:30 +0700

    add asset
    add asset

---
+++ Assets/CryroController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CryroController : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    Collider2D col;
+
+    public GameObject soldier;
+
+    public float moveSpeed;
+    public bool goToLeft;
+    private float solderDelay = 500;
+    private int numOfSolider = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+        SpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!goToLeft)
+        {
+            rb.velocity = new Vector2(moveSpeed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-moveSpeed, 0);
+        }
+
+        solderDelay--;
+        if (solderDelay < 0 && numOfSolider < 3)
+        {
+            Instantiate(soldier, transform.position, Quaternion.identity);
+            solderDelay = 500;
+            numOfSolider++;
+        }
+    }
+
+    public void SpawnPos()
+    {
+        int r = Random.Range(2, 5);
+        int lr = Random.Range(0, 2);
+        int z = Random.Range(0, 1);
+        if(lr == 0)
+        {
+            float left = Random.Range(-12, -10);
+            gameObject.transform.position = new Vector3(left, r, z);
+            goToLeft = false;
+        }
+        else
+        {
+            float right = Random.Range(10, 12);
+            gameObject.transform.position = new Vector3(right, r, z);
+            goToLeft = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "GryroSpawnBox")
+        {
+            Destroy(gameObject);
+        }
+        if (collision.tag == "Gryro" && collision.transform.position.z == gameObject.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Firer")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PlayerContoller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    public GameObject gun;
+    public GameObject firer;
+    public GameObject ciu;
+    public float speed;
+    public static PlayerContoller instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A) && gun.transform.rotation.z < 0.7)
+        {
+            gun.transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D) && gun.transform.rotation.z > -0.7)
+        {
+            gun.transform.Rotate(-Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Instantiate(firer, ciu.transform.position, ciu.transform.rotation);
+            
+        }    
+    }
+}
