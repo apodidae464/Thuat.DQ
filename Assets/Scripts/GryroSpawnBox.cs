d54fca000b1d75c324a67a4fce08c670effe9938
apodidae464 <kimikissmicki@gmail.com>
2021-06-11 17:08:26 +0700

    add scripts folder

---
+++ Assets/Scripts/GryroSpawnBox.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GryroSpawnBox : MonoBehaviour
+{
+    
+    public GameObject gryro;
+
+    public int numOfGryPerFrame;
+
+    int count = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(gryro);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        count--;
+        if(count < 0)
+        {
+            int time = Random.Range(1, 4);
+            for (int i = 0; i < time; i ++)
+            {
+                Instantiate(gryro);
+            }
+            count = 1000;
+        }
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/Soldier.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Collider2D col;
+    bool onGround = false;
+    public Animator anim;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("onGround", onGround);
+        if (onGround && PlayerContoller.instance)
+        {
+            if(transform.position.x < PlayerContoller.instance.transform.position.x)
+            {
+                rb.velocity = new Vector2(speed, 0);
+            } 
+            else
+            {
+                rb.transform.localScale = new Vector3(-1, 1, 1);
+                rb.velocity = new Vector2(-speed, 0);
+
+            }
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(-transform.up);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Firer" || collision.tag == "Soldier")
+        {
+            Destroy(gameObject);
+        }
+        if(collision.tag == "something")
+        {
+            onGround = true;
+        }
+    }
+
+    
+}
